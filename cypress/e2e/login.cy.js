7a78209baa89b9cb8511f849ea068f230beccb7d
Elmira Guseynova <Gelmira5119@gmail.com>
2025-05-16 21:38:07 +0300

    login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,8 +3,8 @@ describe('Проверка авторизации', function () {
     it('Верный пароль и верный логин', function () {
          cy.visit('/');
          cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // проверка цвета кнопки "Забыли пароль"
-         cy.get('#mail').type('german@dolnikov.ru'); //найти поле логин ввести правильный логин
-         cy.get('#pass').type('iLoveqastudio1'); // найти поле пароль ввести правильный пароль
+         cy.get('#mail').type('USER_LOGIN'); //найти поле логин ввести правильный логин
+         cy.get('#pass').type('USER_PASSWORD'); // найти поле пароль ввести правильный пароль
          cy.get('#loginButton').click(); // найти кнопку "войти", нажать на нее
          cy.get('#messageHeader').should('be.visible'); // проверка, что текст с успешной авторизацией виден польз-лю
          cy.get('#messageHeader').contains('Авторизация прошла успешно'); //проверка, что элемент содержит нужный текст
@@ -16,7 +16,7 @@ describe('Проверка авторизации', function () {
         cy.visit('/');
         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // проверка цвета кнопки "Забыли пароль"
         cy.get('#forgotEmailButton').click(); // найти кнопку "забыли пароль", нажать на нее
-        cy.get('#mailForgot').type('german@dolnikov.ru'); // ввели почту
+        cy.get('#mailForgot').type('USER_LOGIN'); // ввели почту
         cy.get('#restoreEmailButton').click(); // нажали кнопку "отправить код"
         cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail'); //проверка, что элемент содержит нужный текст
         cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // наличие кнопки крестик и он виден для польз-ля 
@@ -25,8 +25,8 @@ describe('Проверка авторизации', function () {
        it('Верный логин и неверный пароль', function () {
          cy.visit('/');
          cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');  // проверка цвета кнопки "Забыли пароль"
-         cy.get('#mail').type('german@dolnikov.ru'); //найти поле логин ввести правильный логин
-         cy.get('#pass').type('iLoveqastudio2'); // найти поле пароль ввести НЕ правильный пароль
+         cy.get('#mail').type('USER_LOGIN'); //найти поле логин ввести правильный логин
+         cy.get('#pass').type('USER_PASSWORD'); // найти поле пароль ввести НЕ правильный пароль
          cy.get('#loginButton').click(); // найти кнопку "войти", нажать на нее
          cy.get('#messageHeader').should('be.visible'); // проверка, что текст с НЕ успешной авторизацией виден польз-лю
          cy.get('#messageHeader').contains('Такого логина или пароля нет'); //проверка, что элемент содержит нужный текст
@@ -36,8 +36,8 @@ describe('Проверка авторизации', function () {
      it('Не верный логин и верный пароль', function () {
         cy.visit('/');
         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');  // проверка цвета кнопки "Забыли пароль"
-        cy.get('#mail').type('alex@green.ru'); //найти поле логин ввести НЕ правильный логин
-        cy.get('#pass').type('iLoveqastudio1'); // найти поле пароль ввести правильный пароль
+        cy.get('#mail').type('USER_LOGIN'); //найти поле логин ввести НЕ правильный логин
+        cy.get('#pass').type('USER_PASSWORD'); // найти поле пароль ввести правильный пароль
         cy.get('#loginButton').click(); // найти кнопку "войти", нажать на нее
         cy.get('#messageHeader').should('be.visible'); // проверка, что текст с НЕ успешной авторизацией виден польз-лю
         cy.get('#messageHeader').contains('Такого логина или пароля нет'); //проверка, что элемент содержит нужный текст
@@ -47,8 +47,8 @@ describe('Проверка авторизации', function () {
        it('Валидация на наличие @', function () {
          cy.visit('/');
          cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // проверка цвета кнопки "Забыли пароль"
-         cy.get('#mail').type('germandolnikov.ru'); //найти поле логин ввести НЕ правильный логин - без @
-         cy.get('#pass').type('iLoveqastudio1'); // найти поле пароль ввести правильный пароль
+         cy.get('#mail').type('USER_LOGIN'); //найти поле логин ввести НЕ правильный логин - без @
+         cy.get('#pass').type('USER_PASSWORD'); // найти поле пароль ввести правильный пароль
          cy.get('#loginButton').click(); // найти кнопку "войти", нажать на нее
          cy.get('#messageHeader').should('be.visible'); // проверка, что текст с проблемой валидации виден польз-лю
          cy.get('#messageHeader').contains('Нужно исправить проблему валидации'); //проверка, что элемент содержит нужный текст
@@ -58,8 +58,8 @@ describe('Проверка авторизации', function () {
      it('Приведение к строчным буквам в логине', function () {
         cy.visit('/');
         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); // проверка цвета кнопки "Забыли пароль"
-        cy.get('#mail').type('GerMan@Dolnikov.ru'); //найти поле логин ввести GerMan@Dolnikov.ru
-        cy.get('#pass').type('iLoveqastudio1'); // найти поле пароль ввести правильный пароль
+        cy.get('#mail').type('USER_LOGIN'); //найти поле логин ввести GerMan@Dolnikov.ru
+        cy.get('#pass').type('USER_PASSWORD'); // найти поле пароль ввести правильный пароль
         cy.get('#loginButton').click(); // найти кнопку "войти", нажать на нее
         cy.get('#messageHeader').should('be.visible'); // проверка, что текст с проблемой валидации виден польз-лю
         cy.get('#messageHeader').contains('Авторизация прошла успешно'); //проверка, что элемент содержит нужный текст
